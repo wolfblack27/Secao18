5d580bfefd2122289c207c90a411e1bc5798f9dc
wolfblack27 <tigowolfblack27@gmail.com>
2023-11-10 21:28:08 -0300

    Alteracoes do Projeto

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,13 @@
 package aplication;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
 import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
 
 public class Program {
 
@@ -13,6 +17,17 @@ public class Program {
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		System.out.println("----Entre com os dados do aluguel----");
 		System.out.println("Modelo do Carro: ");
+		String carModel = sc.nextLine();
+		
+		System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(),fmt);
+		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(),fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		sc.close();
+		
 
 	}
 
